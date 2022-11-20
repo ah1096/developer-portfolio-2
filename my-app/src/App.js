af95311a9e63146df36bf67caeade573ca99ff8e
ah1096 <alyssa.holbert@student.awesomeinc.org>
2022-11-20 16:32:40 +0000

    edited Contact text

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -250,7 +250,7 @@ function App() {
   <div className="px-4 py-5 my-5 text-center">
       <h1 id="ContactSection" className="display-5 fw-bold">Contact Me</h1>
           <div className="col-lg-6 mx-auto">
-              <p className="lead mb-4">Questions, offers, advice? Get in touch with me.</p>
+              <p className="lead mb-4">Questions, offers, advice? Want a website made? Get in touch with me.</p>
               <a className="btn btn-primary">say hi</a>
           </div>
   </div>
